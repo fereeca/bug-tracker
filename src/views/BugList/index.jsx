31831169ba42b61a349a14233e92955a460150d7
Fereeca Tuladhar <fereeca.tuladhar@gmail.com>
2023-12-06 12:25:06 +0545

    Table update

--- src/views/BugList/index.jsx
+++ src/views/BugList/index.jsx
@@ -9,8 +9,52 @@ function BugList() {
       <div className="layout">
         <Navbar />
         <div className="inner-layout">
-          <Sidebar />
-          <div>Bug List</div>
+          <div className="side">
+            {" "}
+            <Sidebar />
+          </div>
+          <div className="bug">
+            <div className="submit-bug">
+              <button>Submit Bug</button>
+            </div>
+            <table className="category-table">
+              <tr>
+                <th>Bug Category</th>
+                <th>Title</th>
+                <th>Description</th>
+              </tr>
+              <tr>
+                <td>Bug ID</td>
+                <td>1</td>
+                <td>Unable to add new item in cart</td>
+              </tr>
+              <tr>
+                <td>Bug ID</td>
+                <td>1</td>
+                <td>Unable to add new item in cart</td>
+              </tr>
+              <tr>
+                <td>Bug ID</td>
+                <td>1</td>
+                <td>Unable to add new item in cart</td>
+              </tr>
+              <tr>
+                <td>Bug ID</td>
+                <td>1</td>
+                <td>Unable to add new item in cart</td>
+              </tr>
+              <tr>
+                <td>Bug ID</td>
+                <td>1</td>
+                <td>Unable to add new item in cart</td>
+              </tr>
+              <tr>
+                <td>Bug ID</td>
+                <td>1</td>
+                <td>Unable to add new item in cart</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </>
